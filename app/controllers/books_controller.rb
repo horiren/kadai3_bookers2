86381648293f84a55fc5307594e3f09eebeac666
horiren <138359070+horiren@users.noreply.github.com>
2023-07-22 08:21:21 +0000

    bookers2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,34 @@
 class BooksController < ApplicationController
   @books = Book.new
+  
+  def index
+    @books = Book.all
+    @users = current_user
+    @book = Book.new
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+  end
+  
+  def edit
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
+  def destroy
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @books = Book.all
+    @users = current_user
+    @book = Book.new
   end
 
   def edit
+  end
+  
+  def index
+    @users = User.all
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 end
